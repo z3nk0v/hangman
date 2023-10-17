2b1ebcb45b72314c13cef3aba0d79eed369121fc
Dmitry Rukazenkov <dmitry@pc-11-251.customer.ask4.lan>
2023-10-17 20:41:56 +0100

    updated milestone 4

--- milestone_4.py
+++ milestone_4.py
@@ -2,36 +2,38 @@ import random
 
 class hangman:
     def __init__(self, word_list, num_lives = 5) -> None:
-        word = random.choice(word_list)
-        word_guessed = len(word) * ['_']
-        num_letters = len(set(word))
-        num_lives = num_lives
-        list_of_guesses = []
+        self.word = random.choice(word_list)
+        self.word_guessed = len(self.word) * ['_']
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.list_of_guesses = []
 
         def check_guess(guess):
             guess = guess.lower()
-            if guess in word:
+            if guess in self.word:
                 print(f'Good guess! {guess} is in the word.')
-                for letter in word:
+                for letter in self.word:
                     if guess == letter:
-                        word_guessed[word.index(guess)] = guess
-                num_letters -= 1
-                print(word_guessed)
+                        self.word_guessed[self.word.index(guess)] = guess
+                self.num_letters -= 1
+                print(self.word_guessed)
             else:
                 print(f'Sorry, {guess} is not in the word. Try again')
+                self.num_lives -=1
+                print(f"You have {self.num_lives} lives left.")
 
         def ask_for_input():
             print("Please guess a letter")
-            print(num_letters)
+            print(self.num_letters)
             while True:
                 guess = input()
                 if not (guess.isalpha() and len(guess) == 1) :
                     print("Invalid letter. Please, enter a single alphabetical character.")
-                elif guess in list_of_guesses:
+                elif guess in self.list_of_guesses:
                     print("You already tried that letter!")
                 else:
                     check_guess(guess)
-                    list_of_guesses.append(guess)         
+                    self.list_of_guesses.append(guess)         
 
 
         ask_for_input()
