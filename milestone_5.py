a70571308e687c063066ebc3466eb5fb1ac67d7f
Dmitry Rukazenkov <dmitry@Dmitrys-MacBook-Air.local>
2023-10-19 16:50:59 +0100

    Uploaded updates with docustrings

--- milestone_5.py
+++ milestone_5.py
@@ -1,6 +1,8 @@
 import random
 
 class hangman:
+    '''Defines our class and all the variables we will be using inside'''    
+   
     def __init__(self, word_list, num_lives = 5) -> None:
         self.word = random.choice(word_list)
         self.word_guessed = len(self.word) * ['_']
@@ -8,7 +10,10 @@ class hangman:
         self.num_lives = num_lives
         self.list_of_guesses = []
 
+
     def check_guess(self, guess):
+        '''this is our check guess function which checks the inputted letter against our chosen word'''
+        
         guess = guess.lower()
         if guess in self.word:
             print(f'Good guess! {guess} is in the word.')
@@ -18,13 +23,19 @@ class hangman:
             self.num_letters -= 1
             print(self.word_guessed)
             print(self.num_letters)
+
         else:
+            '''this code runs if the imputted letter is not in the chosen word and prints a message'''
+            
             print(f'Sorry, {guess} is not in the word. Try again')
             self.num_lives -=1
             print(f"You have {self.num_lives} lives left.")
             print(self.word_guessed)
 
+
     def ask_for_input(self):
+        '''Checks whether input is a single alphabetical letter and if has already been inputted'''
+
         print("Please guess a letter")
         guess = input()
         if not (guess.isalpha() and len(guess) == 1) :
@@ -37,6 +48,8 @@ class hangman:
 
 
 def play_game(word_list):
+    '''This code executes the game with the starting lives at 5 and also dictates what happens when the lives run out or the word is guessed'''
+
     num_lives = 5
     game = hangman(word_list, num_lives)
     while True:
@@ -49,4 +62,5 @@ def play_game(word_list):
                 print('Congratulations. You won the game!') 
                 break
         
-play_game(['last','john','grey'])
+play_game(['dmitry','chris','patrick'])
+'''Calls the game function with our list of random words'''
